34ecb544a98b7810f16c1a46026ff63fd2e8e891
Sithu <cithukyaw@gmail.com>
2024-05-21 18:47:37 +0630

    [backend] Add config and models with Read API

---
+++ backend/models.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from config import db
+
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    created = db.Column(db.DateTime, unique=False, nullable=False, default=datetime.utcnow)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email
+        }
