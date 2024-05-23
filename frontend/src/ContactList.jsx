65797ffc34fb87a71e9a95b0b05bb5cf9028d3c6
Sithu <cithukyaw@gmail.com>
2024-05-23 16:59:08 +0630

    [frontend] Implement contact delete

--- frontend/src/ContactList.jsx
+++ frontend/src/ContactList.jsx
@@ -1,29 +1,49 @@
 const ContactList = ({ contacts, updateContact, updateCallback }) => {
+    const onDelete = async (id) => {
+        try {
+            const options = {
+                method: 'DELETE',
+            }
+            const response = await fetch(`http://127.0.0.1:5000/contacts/${id}`, options)
+            if (response.status === 200) {
+                updateCallback()
+            } else {
+                console.log(response.message)
+            }
+        } catch (error) {
+            alert(error)
+        }
+    }
+
     return <div>
         <h2>Contact Manager</h2>
-        <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Phone</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                {contacts.map(contact => (
-                    <tr key={contact.id}>
-                        <td>{contact.name}</td>
-                        <td>{contact.email}</td>
-                        <td>{contact.phone}</td>
-                        <td>
-                            <button onClick={() => updateContact(contact)}>Update</button>
-                            <button>Delete</button>
-                        </td>
+        { contacts.length > 0 ?
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Actions</th>
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {contacts.map(contact => (
+                        <tr key={contact.id}>
+                            <td>{contact.name}</td>
+                            <td>{contact.email}</td>
+                            <td>{contact.phone}</td>
+                            <td>
+                                <button onClick={() => updateContact(contact)}>Update</button>
+                                <button onClick={() => onDelete(contact.id)}>Delete</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            :
+            <h3>No contact yet. Create a new contact!</h3>
+        }
     </div>
 }
 
